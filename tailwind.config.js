f152bc3f128e2cd2413daef06eb9ede7393dfbc6
Dhanraj Avhad <dhanraj30102002@gmail.com>
2024-06-27 20:49:01 +0530

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const flattenColorPalette = require("tailwindcss/lib/util/flattenColorPalette").default;
+
+const addVariablesForColors = ({ addBase, theme }) => {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+};
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    // rest of the code
+  },
+  plugins: [addVariablesForColors],
+};
